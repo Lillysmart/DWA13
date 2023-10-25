67ba6185026160701c8ccc3b5e34a0b9f183d06d
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-25 20:23:01 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,54 @@
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
 
 //Console each name
-names.forEach(name => {
-    console.log(name)
+names.forEach((name) => {
+  console.log(name);
 });
 
-//console each province 
-provinces.forEach((provinceName)=>{
-console.log(provinceName)
-})
-
+//console each province
+provinces.forEach((provinceName) => {
+  console.log(provinceName);
+});
 
 //log both the province and name with their indeces.
 names.forEach((name, index) => {
-    const province = provinces[index] || ''; // Use an empty string if the array is shorter
-    console.log(` ${name}( ${province})`);
+  const province = provinces[index] || ""; // Use an empty string if the array is shorter
+  console.log(` ${name}( ${province})`);
 });
-
 
 //Province to uppercase
- provinces.map(province => {
-    
-   console.log(province.toUpperCase()
-   ) 
-   
+provinces.map((province) => {
+  console.log(province.toUpperCase());
 });
 
-
 //Amount of characters in each name
-names.map(name => {
-    
-    console.log(name.length
-    ) 
-    
- });
+names.map((name) => {
+  console.log(name.length);
+});
 
 //Province in alphabetical Order
- const provincesSort =provinces.toSorted ()
- console.log(provincesSort)
- 
+const provincesSort = provinces.toSorted();
+console.log(provincesSort);
 
+
+//Provinces with Cape
+const provincesRemaining = provinces.filter((province) =>
+  province.includes("Cape")
+);
+
+console.log(provincesRemaining);
